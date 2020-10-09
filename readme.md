7d2f64f3e2d1ff8298621cfe4ef4e92f93550612
Dobbyd <maarten.veenstra@hotmail.com>
2020-10-09 11:25:00 +0200

    Add files via upload

--- readme.md
+++ readme.md
@@ -23,13 +23,64 @@
 
 ## Voortgang 3 (week 6)
 
--same as voortgang 1-
+### Stand van zaken
+
+-De nav balk is nu semantisch correcter, geen links maar een button aangezien het iets op de pagina zelf doet. Themeswitcher is nog niet optimaal aangezien ik met important werk. De menuknop opent nu een menu zoals op de originele site.-
+
+**Screenshot(s):**
+
+![Screenshot1](/images/Screenshot1 "screenshot1")
+![Screenshot2](/images/Screenshot2 "screenshot2")
+
+### Agenda voor meeting
+
+-Ik kan geen goede manier verzinnen om de theme switcher handiger te laten werken, ik mis een puzzelstukje dat ik nodig heb om dit slim te laten werken.-
+
+### Verslag van meeting
+
+--
 
 
 
 ## Voortgang 2 (week 5)
 
--same as voortgang 1-
+### Stand van zaken
+
+-Ik heb een darktheme/lighttheme switcher toegevoegd, deze werkt echter met !important wat niet goed is. Verder heb ik 
+een pagina gemaakt die nog niet ingevuld is.-
+
+**Screenshot(s):**
+
+![Screenshot1](/images/Screenshot1 "screenshot1")
+![Screenshot2](/images/Screenshot2 "screenshot2")
+
+### Agenda voor meeting
+
+-Geen agenda voor meeting.-
+
+### Verslag van meeting
+
+-feedback van Robert: 
+body class main zodat je body.main header kan aanroepen in css 
+
+https://www.w3schools.com/css/css_attribute_selectors.asp
+op basis van bv een href een specifiek iets kunnen aanroepen
+bijvoorbeeld
+a[href="index.html]{
+color= black;
+}
+
+div niet nodig misschien 
+alt teksten "vrouw drinkt cola afbeeldingen bv"
+id naam veranderen 
+uppercase ipv all caps overal 
+
+const gebruiken ipv vars
+
+
+padding bottom 10px 
+
+line height percentages ipv procenten -
 
 
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,68 @@
-// JavaScript Document
-//dark theme light theme switch
-
+// JavaScript Document//
+// eslint-env es6 //
+// eslint-disable no-console //
+//deze tekst is om errors te vermijden//
 
 var listTheme = document.querySelector("ul");
+var themeButton = document.querySelector("li:nth-of-type(4) > button");
+var themeButtonTwo = document.querySelector("li:first-of-type > button");
+var footerTheme = document.querySelectorAll("section:last-of-type > a");
 var linkTheme = document.querySelectorAll("a");
-var themeButton = document.querySelector("button");
-var footerTheme = document.querySelectorAll("section:nth-of-type(11) > a");
-
+//ik ben me bewust van het feit dat dit const moet zijn maar ik krijg parsing errors, met var werkt het wel//
 
 
 themeButton.addEventListener("click", function () {
 
     "use strict";
-    //rare error in brackets als ik dit niet doe
+    //ik krijg een JSLint error zonder deze tekst//
 
     listTheme.classList.toggle("light");
     linkTheme[0].classList.toggle("light");
     linkTheme[1].classList.toggle("light");
-    linkTheme[2].classList.toggle("light");
     themeButton.classList.toggle("dark");
+    themeButtonTwo.classList.toggle("light");
     footerTheme[0].classList.toggle("light");
     footerTheme[1].classList.toggle("light");
     footerTheme[2].classList.toggle("light");
 
+
+});
+
+
+var navMenu = document.querySelector("li:first-of-type > button");
+var dropMenu = document.querySelector("ul:nth-of-type(2)");
+var menuFocus = document.querySelector(".unhideMenu");
+var everyBody = document.querySelector("body");
+var listGone = document.querySelector(".hideMenu");
+
+everyBody.addEventListener("keydown", function () {
+    "use strict";
+    if (event.keyCode === 9) {
+        if (menuFocus === document.activeElement) {
+            dropMenu.classList.add("listOn");
+        }
+        if (listGone === document.activeElement) {
+            dropMenu.classList.remove("listOn");
+        }
+
+    }
+});
+/* Door een niet zichtbaar menu tabben is een beetje vreemd, hier zorg ik ervoor dat het menu ook uitklapt wanneer de focus op het menu komt. Aangezien de functie pas wordt getriggered wanneer er op tab wordt gedrukt zit de class op het laatste object in de zichtbare navbar, wanneer er dan getabd word staat de focus op de eerste item in de nav en wordt deze zichtbaar. Wanneer er door het menu getabbed is wordt hetzelfde trucje gebruikt om het menu weer in te klappen. */
+
+
+navMenu.addEventListener("click", function () {
+    "use strict";
+    if (dropMenu.classList.contains("listOn")) {
+        dropMenu.classList.remove("listOn");
+    } else {
+        dropMenu.classList.add("listOn");
+    }
+
+
+    /*if (dropMenu.style.display === "none") {
+        dropMenu.style.display = "block";
+    } else {
+        dropMenu.style.display = "none";
+    }*/
 
 });
